bce9810d4562bb2fee82b0fd6021830567503fc6
jongmin96 <jitae01@gachon.ac.kr>
2023-11-24 02:19:29 +0900

    download2

--- src/main/java/com/anything/gradproject/controller/TeacherController.java
+++ src/main/java/com/anything/gradproject/controller/TeacherController.java
@@ -5,13 +5,17 @@ import com.anything.gradproject.entity.*;
 import com.anything.gradproject.repository.*;
 import com.anything.gradproject.service.*;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 import static org.thymeleaf.util.StringUtils.substring;
